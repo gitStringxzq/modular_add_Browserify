68c4cf4b2de739776321ff4dce907e3eccab68ff
xzq <944729356@qq.com>
2021-04-07 12:43:39 +0800

    this is 4-7 12.43  add

--- ES6-babel-browserify-fuxi/js/src/module2.js
+++
@@ -1,7 +0,0 @@
-//分别暴露
-export function demo2(){
-    console.log('我是module2');
-}
-export function test2 (){
-    console.log('我也是module2');
-}
--- ES6-babel-browserify-fuxi/js/src/module3.js
+++
@@ -1,8 +0,0 @@
-export default {
-    name:'xzq',
-    age:12,
-    spack(){
-        console.log(`你好的我名字是${this.name}我的年龄是${this.age}`)
-    },
-    arr:[1,2,32,32,4,23,2,13,1231,23,2,3,23,2,3]
-}
